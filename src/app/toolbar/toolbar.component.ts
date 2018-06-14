ab9ca595b5b929e6af5d23ee924042062a7d061a
Michael <michaelblxck@gmail.com>
2018-06-13 21:19:45 -0700

    added toolbar animation

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  showAnimation():boolean {
+    if(this.router.url === '/') return false;
+    else return true;
   }
 
 }
